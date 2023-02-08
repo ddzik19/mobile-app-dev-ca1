4c7e90207afd17d1f4692cff5dd334cabbecec61
ddzik19 <ddzik09@gmail.com>
2023-02-08 14:25:46 +0000

    removed actionbar

--- ca1/app/src/main/java/ie/wit/ca1/activities/CardCollectionActivity.kt
+++ ca1/app/src/main/java/ie/wit/ca1/activities/CardCollectionActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ie.wit.ca1.R
 
-class MainActivity : AppCompatActivity() {
+class CardCollectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.card_collection_activity)
     }
 }
