6909c7591e4f391735aa397ba95dff94941d88cf
ddzik19 <ddzik09@gmail.com>
2023-02-28 10:30:16 +0000

    new activity
    This activity will be used to create cards which then will be added into the collection

---
+++ ca1/app/src/main/java/ie/wit/ca1/activities/AddCardActivity.kt
@@ -0,0 +1,12 @@
+package ie.wit.ca1.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.wit.ca1.R
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+    }
+}
